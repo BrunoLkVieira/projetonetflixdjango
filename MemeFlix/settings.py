c0e11e005551123c5b6c6866e2f01a3b834898d1
BrunoLK021 <brunoalencarvieira136@gmail.com>
2023-05-27 15:13:42 -0300

    update DATABASE_URL

--- MemeFlix/settings.py
+++ MemeFlix/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
 import dj_database_url
 import os
 
-DATABASE_URL = os.getnv("DATABASE_URL")
-if CATABASE_URL:
+DATABASE_URL = os.getenv("DATABASE_URL")
+if DATABASE_URL:
     DATABASE = {
         "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800)
     }
